8d5eeac23c6470852f7b5c3a6020b0e1fe88647b
valerojasu <valerojasu98@hotmail.com>
2023-04-23 21:06:47 -0600

    4rd

--- RepositorioPacientes/src/Formulario.jsx
+++ RepositorioPacientes/src/Formulario.jsx
@@ -23,11 +23,20 @@ function Formulario(){
         <div className="Formulario">
             <h1 className="titulo">CREAR CITAS</h1>
         <form className="inputForm" action="">
-            <label className="Formulariotext" htmlFor="" >Nombre mascota</label>
-            <input className="Formulariobox" onChange={(e) =>{setMascota(e.target.value)}} value={mascota} type="text"></input>
+        
+            <label className="Formulariotext" htmlFor="" >Nombre Mascota</label>
+            <input className="Formulariobox" onChange={(e) =>{setMascota(e.target.value)}} value={mascota} type="text" placeholder="Nombre Mascota"></input>
             <label className="Formulariotext" htmlFor="">Nombre Dueño</label>
-            <input className="Formulariobox" onChange={(e) =>{setDueño(e.target.value)}} value={dueño} type="text"></input>
-            <button className="boton" onClick={agregarPaciente} type="submit"> Agregar </button>
+            <input className="Formulariobox" onChange={(e) =>{setDueño(e.target.value)}} value={dueño} type="text" placeholder="Nombre Dueño de la mascota"></input>
+            <label className="Formulariotext" htmlFor="">Fecha</label>
+            <input className="Formulariobox"  type="date" name="bday" />
+            <label className="Formulariotext" htmlFor="">Hora</label>
+            <input className="Formulariobox" type="time"></input>
+            <label className="Formulariotext" htmlFor="">Sintomas</label>
+            <textarea className="Formulariotbox" id="exampleFormControlTextarea1" rows="3"></textarea>
+
+
+            <button className="boton" onClick={agregarPaciente} type="submit"> Agregar Cita </button>
         </form>                    
         
         </div>
