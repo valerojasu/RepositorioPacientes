bc712544aaa137f33c6edde89772d56dd5744ddd
valerojasu <valerojasu98@hotmail.com>
2023-04-25 19:32:52 -0600

    2nda Fase

--- RepositorioPacientes/src/Formulario.jsx
+++ RepositorioPacientes/src/Formulario.jsx
@@ -5,16 +5,24 @@ function Formulario(){
     const[mascota,setMascota]=useState("")
     const[dueño,setDueño]=useState("")
     const[paciente,setPaciente]=useState([])
+    const [fecha, setFecha]=useState("")
+    const [hora, setHora]=useState("")
+    const [sintoma, setSinto]=useState("")
 
     function agregarPaciente(){
-        const informacion={
+        const informacion=(
             mascota,
-            dueño
-        }
+            dueño,
+            fecha,
+            hora,
+            sintoma
+         
+        )
 
         setPaciente([...paciente,informacion])
         setMascota("")
         setDueño("")
+        setSinto("")
 
     }
 
@@ -23,17 +31,20 @@ function Formulario(){
         <div className="Formulario">
             <h1 className="titulo">CREAR CITAS</h1>
         <form className="inputForm" action="">
-        
             <label className="Formulariotext" htmlFor="" >Nombre Mascota</label>
             <input className="Formulariobox" onChange={(e) =>{setMascota(e.target.value)}} value={mascota} type="text" placeholder="Nombre Mascota"></input>
+
             <label className="Formulariotext" htmlFor="">Nombre Dueño</label>
             <input className="Formulariobox" onChange={(e) =>{setDueño(e.target.value)}} value={dueño} type="text" placeholder="Nombre Dueño de la mascota"></input>
+            
             <label className="Formulariotext" htmlFor="">Fecha</label>
-            <input className="Formulariobox"  type="date" name="bday" />
+            <input className="Formulariobox"  onChange={(e) =>{setFecha(e.target.value)}} value={fecha} type="date" name="bday" />
+            
             <label className="Formulariotext" htmlFor="">Hora</label>
-            <input className="Formulariobox" type="time"></input>
+            <input className="Formulariobox" onChange={(e) =>{setHora(e.target.value)}} value={hora}type="time"></input>
+            
             <label className="Formulariotext" htmlFor="">Sintomas</label>
-            <textarea className="Formulariotbox" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <textarea className="Formulariotbox" onChange={(e) =>{setSinto(e.target.value)}} value={sintoma}id="exampleFormControlTextarea1" rows="3"></textarea>
 
 
             <button className="boton" onClick={agregarPaciente} type="submit"> Agregar Cita </button>
